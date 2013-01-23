038428b8ed01159cbb2e0a79da515c329e9c1f2a
shtlee <shtlee@qiniu.(none)>
2013-01-23 16:10:39 +0800

    modify for tool_qetag

--- qetag.go
+++ qetag.go
@@ -20,47 +20,56 @@ func BlockCount(fsize int64) int {
 	return int((fsize + blockMask) >> BLOCK_BITS)
 }
 
-func CalSha1(r io.Reader) []byte {
+func CalSha1(r io.Reader) (sha1Code []byte, err error) {
 	h := sha1.New()
-	_, err := io.Copy(h, r)
+	_, err = io.Copy(h, r)
 	if err != nil {
-		log.Println("Calculate sha1 error: ", err)
-		return nil
+		return
 	}
-	return h.Sum(nil)
+	sha1Code = h.Sum(nil)
+	return 
 }
 
 func GetEtag(filename string) (etag string, err error) {
 	f, err := os.Open(filename)
 	if err != nil {
-		log.Println("Open file error : ", err)
 		return
 	}
 	defer f.Close()
+
 	fi, err := f.Stat()
 	if err != nil {
-		log.Println(err)
 		return
 	}
 	fsize := fi.Size()
 	blockCnt := BlockCount(fsize)
 	sha1Buf := make([]byte, 0, 21)
 
+	var sha1Code []byte
 	if blockCnt <= 1 { // file size <= 4M
 		sha1Buf = append(sha1Buf, 0x16)
-		sha1Code := CalSha1(f)
+		sha1Code, err = CalSha1(f) 
+		if err != nil {
+			return
+		}
 		sha1Buf = append(sha1Buf, sha1Code...)
 	} else { // file size > 4M
 		sha1Buf = append(sha1Buf, 0x96)
 		sha1BlockBuf := make([]byte, 0, blockCnt * 20)
-
 		for i := 0; i < blockCnt; i ++ {
 			body := io.LimitReader(f, 1 << BLOCK_BITS)
-			sha1Code := CalSha1(body)
+			sha1Code, err = CalSha1(body)
+			if err != nil {
+				return
+			}
 			sha1BlockBuf = append(sha1BlockBuf,sha1Code...)
 		}
 		tmpBuf := bytes.NewBuffer(sha1BlockBuf)
-		sha1Final := CalSha1(tmpBuf)
+		var sha1Final []byte
+		sha1Final, err = CalSha1(tmpBuf)
+		if err != nil {
+			return
+		}
 		sha1Buf = append(sha1Buf, sha1Final...)
 	}
 	etag = base64.URLEncoding.EncodeToString(sha1Buf)
